cc809c05c03e5318483cdab51e6c88566d23c177
HugoRCD <hugo.richard@epitech.eu>
2023-03-20 16:11:20 +0100

    Update name and favicon

---
+++ app.config.ts
@@ -0,0 +1,6 @@
+export default defineAppConfig({
+  appName: "helpr",
+  appTitle: "Helpr",
+  appDescription: "Helpr is a platform that bring your workflow to the next level.",
+  appVersion: "1.0.0",
+});
--- app.vue
+++ app.vue
@@ -1,3 +1,30 @@
+<script setup lang="ts">
+const { locale } = useI18n();
+
+useHead({
+  title: useAppConfig().appTitle,
+  meta: [
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { name: "description", content: useAppConfig().appDescription },
+    { name: "keywords", content: "Home" },
+  ],
+  link: [
+    {
+      rel: "icon",
+      type: "image/x-icon",
+      href: "/favicon.ico",
+    },
+  ],
+});
+
+onMounted(() => {
+  const userLocale = useLocalStorage("helpr_locale", "en");
+  const userTheme = useLocalStorage("helpr_theme", "cosmos");
+
+  locale.value = userLocale.value;
+});
+</script>
+
 <template>
   <div>
     <NuxtWelcome />
