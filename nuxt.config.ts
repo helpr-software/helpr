56b9b4f6a4681b3115ee9fa437bb3ba631c55f10
HugoRCD <hugo.richard@epitech.eu>
2023-03-20 16:09:46 +0100

    Init Helpr V2

---
+++ nuxt.config.ts
@@ -0,0 +1,90 @@
+import en from "./locales/en.json";
+import fr from "./locales/fr.json";
+
+export default defineNuxtConfig({
+  app: {
+    layoutTransition: {
+      name: "fade",
+      mode: "out-in",
+    },
+    pageTransition: {
+      name: "fade",
+      mode: "out-in",
+    },
+  },
+  css: ["~/assets/style/main.scss"],
+
+  build: {
+    transpile: ["@heroicons/vue"],
+  },
+
+  imports: {
+    dirs: ["store"],
+  },
+
+  modules: [
+    "@nuxt/image-edge",
+    "@nuxtjs/i18n",
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "@nuxt/content",
+    "@nuxtjs/supabase",
+    "@pinia/nuxt",
+    "nuxt-headlessui",
+    "nuxt-mailer",
+  ],
+
+  runtimeConfig: {
+    mailerUser: process.env.MAILER_USER,
+    mailerPass: process.env.MAILER_PASSWORD,
+    private: {
+      stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+      authSecret: process.env.AUTH_TOKEN_SECRET,
+      refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
+      openAiKey: process.env.OPENAI_API_KEY,
+    },
+    public: {
+      googleClientId: process.env.GOOGLE_CLIENT_ID,
+      github: {
+        clientId: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackUrl: process.env.GITHUB_CALLBACK_URL,
+      },
+      linear: {
+        apiKey: process.env.LINEAR_API_KEY,
+        clientId: process.env.LINEAR_CLIENT_ID,
+        clientSecret: process.env.LINEAR_CLIENT_SECRET,
+        callbackUrl: process.env.LINEAR_CALLBACK_URL,
+      },
+      linkedin: {
+        clientId: process.env.LINKEDIN_CLIENT_ID,
+        clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+        callbackUrl: process.env.LINKEDIN_CALLBACK_URL,
+      },
+      appDomain: process.env.FRONTEND_URL,
+      appEnv: process.env.APP_ENV,
+      apiUrl: process.env.API_URL,
+    },
+  },
+
+  i18n: {
+    vueI18n: {
+      legacy: false,
+      locale: "en",
+      fallbackLocale: "en",
+      availableLocales: ["en", "fr"],
+      messages: {
+        en,
+        fr,
+      },
+    },
+  },
+
+  image: {
+    alias: {
+      supabase: "https://uynsrkwqyplqhfrmdaqw.supabase.co/storage/v1/object/public",
+    },
+    domains: ["uynsrkwqyplqhfrmdaqw.supabase.co"],
+    dir: "assets/media",
+  },
+})
