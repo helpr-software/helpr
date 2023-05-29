9b2a2aca7b049d68f8f2c900c054837d1f2c6a72
HugoRCD <hugo.richard@epitech.eu>
2023-05-29 14:49:05 +0200

    Start adding nuxt-ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 import messages from "./locales/messages";
 
+function withOpacity(variableName: string) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`;
+    }
+    return `rgb(var(${variableName}))`;
+  };
+}
+
 export default defineNuxtConfig({
   app: {
     layoutTransition: {
@@ -17,16 +26,7 @@ export default defineNuxtConfig({
     dirs: ["store"],
   },
 
-  modules: [
-    "nuxt-vue3-google-signin",
-    "@nuxt/image-edge",
-    "@nuxtjs/i18n",
-    "@nuxtjs/tailwindcss",
-    "@vueuse/nuxt",
-    "@pinia/nuxt",
-    "nuxt-headlessui",
-    "nuxt-mailer",
-  ],
+  modules: ["nuxt-vue3-google-signin", "@nuxt/image-edge", "@nuxtjs/i18n", "@vueuse/nuxt", "@pinia/nuxt", "nuxt-headlessui", "nuxt-mailer", "@nuxthq/ui"],
 
   googleSignIn: {
     clientId: process.env.GOOGLE_CLIENT_ID,
@@ -70,4 +70,58 @@ export default defineNuxtConfig({
   },
 
   plugins: [{ src: "~/plugins/vercel.ts", mode: "client" }],
+
+  tailwindcss: {
+    cssPath: "~/assets/style/main.scss",
+    config: {
+      content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./nuxt.config.{js,ts}",
+        "./app.vue",
+        "./error.vue",
+      ],
+      theme: {
+        extend: {
+          colors: {
+            primary: "var(--bg-primary)",
+            "primary-opacity": withOpacity("--bg-primary-opacity"),
+            secondary: "var(--bg-secondary)",
+            accent: withOpacity("--accent-color"),
+            "accent-hover": withOpacity("--accent-color-hover"),
+            "accent-faded": "var(--accent-color-faded)",
+            muted: "var(--font-muted)",
+            placeholder: "var(--font-placeholder)",
+          },
+          textColor: {
+            primary: "var(--font-primary)",
+            muted: "var(--font-muted)",
+            placeholder: "var(--font-placeholder)",
+            inverted: "var(--font-inverted)",
+            accent: withOpacity("--accent-color"),
+            "accent-hover": withOpacity("--accent-color-hover"),
+          },
+          borderColor: {
+            primary: "var(--border-primary)",
+            "primary-hover": "var(--border-primary-hover)",
+            muted: "var(--border-muted)",
+            accent: withOpacity("--accent-color"),
+            "accent-faded": "var(--accent-color-faded)",
+            transparent: "transparent",
+          },
+          gradientColorStops: {
+            "accent-primary": "var(--accent-primary)",
+            "accent-secondary": "var(--accent-secondary)",
+          },
+          borderWidth: {
+            sm: "1px",
+            md: "2px",
+          },
+        },
+      },
+      plugins: [],
+    },
+  },
 });
